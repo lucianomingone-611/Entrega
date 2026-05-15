80cd865efb1076e6d9597bba92a16589caffe0b9
Luciano Mingone <lucianomingone@hotmail.com>
2026-05-15 16:02:20 -0300

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>© 2026 Compumundo Hipermega Red. Todos los derechos reservados</p>
+      <nav>
+        <ul className="nav-list">
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </footer>
+  );
+};
