92497efe4af6eb5f3e7ea3644b843af6a5827db7
Luciano Mingone <lucianomingone@hotmail.com>
2026-05-15 17:13:10 -0300

    Primera modificacion

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import "./Nav.css";
+import { useCart } from "../../context/CartContext";
 
 export const Nav = () => {
+  const { getTotalItems } = useCart();
+
+  const totalIems = getTotalItems();
+
   return (
     <nav>
       {}
@@ -10,7 +15,10 @@ export const Nav = () => {
           <Link to={"/"}>Home</Link>
         </li>
         <li>
-          <Link to={"/carrito"}>Carrito</Link>
+          <Link to={"/carrito"}>
+            Carrito
+            {totalIems > 0 && <span className="incart">{totalIems}</span>}
+          </Link>
         </li>
       </ul>
     </nav>
