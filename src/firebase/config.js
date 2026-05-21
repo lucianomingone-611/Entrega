efb12926cf79873c0871c42ea33e4cfb2a35b7d7
Luciano Mingone <lucianomingone@hotmail.com>
2026-05-21 14:40:35 -0300

    IntegracionFirebase

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAM1tMt_y5lpckj4DHTd3Jm8DEOxGr5NFI",
+  authDomain: "proyectoreact-8bd73.firebaseapp.com",
+  projectId: "proyectoreact-8bd73",
+  storageBucket: "proyectoreact-8bd73.firebasestorage.app",
+  messagingSenderId: "173861708147",
+  appId: "1:173861708147:web:d6eb555b7d5152d659e506",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+export { db };
